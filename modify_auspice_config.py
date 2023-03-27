224ae12a0eda593beac0a27374f895a3fdcb5250
jgallowa07 <jaredgalloway07@gmail.com>
2023-03-27 10:15:43 -0700

    removed coloring for non polyclonal preds

--- modify_auspice_config.py
+++ modify_auspice_config.py
@@ -59,14 +59,14 @@ if __name__ == "__main__":
                 "key" : f"{exp_label}",
                 "title" : f"{exp_label}",
                 "type": "continuous",
-                "scale": [[0.0, "#2B83BA"], [0.5, "#FBC93D"], [1.0, "#D7191C"]],
-                "legend": [
-                    {"value": 0.001, "display": "0.00", "bounds": [0.000, 0.125]},
-                    {"value": 0.25, "display": "0.25", "bounds": [0.125, 0.375]},
-                    {"value": 0.50, "display": "0.50", "bounds": [0.375, 0.625]},
-                    {"value": 0.75, "display": "0.75", "bounds": [0.625, 0.875]},
-                    {"value": 1.00, "display": "1.00", "bounds": [0.875, 1.000]}
-                ]
+                #"scale": [[0.0, "#2B83BA"], [0.5, "#FBC93D"], [1.0, "#D7191C"]],
+                #"legend": [
+                #    {"value": 0.001, "display": "0.00", "bounds": [0.000, 0.125]},
+                #    {"value": 0.25, "display": "0.25", "bounds": [0.125, 0.375]},
+                #    {"value": 0.50, "display": "0.50", "bounds": [0.375, 0.625]},
+                #    {"value": 0.75, "display": "0.75", "bounds": [0.625, 0.875]},
+                #    {"value": 1.00, "display": "1.00", "bounds": [0.875, 1.000]}
+                #]
             })
 
     write_json(auspice_config, args.output_config_path, include_version=False)
